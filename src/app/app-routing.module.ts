702e72d5b3c23e00b3eaf213690496ea1ba995b7
Bilel <maamouri.bilel@gmail.com>
2019-05-18 12:39:17 +0100

    app: Testing/Learning project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [
-  { path: '', component: ListComponent },
-];
+const routes: Routes = [{ path: '', component: ListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
